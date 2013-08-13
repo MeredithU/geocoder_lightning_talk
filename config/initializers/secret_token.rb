00196b015bbaaea2fcacb12ffdc0ca2982628fc8
Meredith Underell <m.underell@gmail.com>
2013-08-13 02:19:17 -0700

    First commit using Geocoder gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeocoderTalk::Application.config.secret_token = '856b53942905d031b0a528cd73b93768931541e19bc8f2c189177fff04524db4f3386ef071fa45e678e9c7ed59d5da0d92b01528e7cccd5af942ef6221882ba1'
